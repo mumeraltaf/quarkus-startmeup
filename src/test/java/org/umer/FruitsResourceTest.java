b1e3bd6add3bf2f696fb5b38bcdae7712f1675dc
Umer <umer.altaf@unimelb.edu.au>
2022-08-27 13:12:04 +1000

    basic docker compose

--- src/test/java/org/umer/FruitsResourceTest.java
+++ src/test/java/org/umer/FruitsResourceTest.java
@@ -1,8 +1,13 @@
 package org.umer;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.umer.domain.Fruit;
+import org.umer.service.FruitService;
 
+
+import javax.inject.Inject;
 
 import static io.restassured.RestAssured.given;
 
@@ -16,5 +21,16 @@ public class FruitsResourceTest {
           .then()
              .statusCode(200);
     }
+    @Inject
+    FruitService fruitService;
 
+
+    @Test
+    public void testAddingAFruit() throws Exception {
+
+        Fruit testFruit = new Fruit("test", "testRedApple","red is test");
+        Fruit insertedFruit = fruitService.add(testFruit);
+        Assertions.assertEquals(insertedFruit.getName(),testFruit.getName());
+
+    }
 }
--- src/test/java/org/umer/NativeFruitsResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.umer;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class NativeFruitsResourceIT extends FruitsResourceTest {
-
-    // Execute the same tests but in native mode.
-}
