8b0bf96b11d570f231e86965f1a07411405a3ca0
Umer <umer.altaf@unimelb.edu.au>
2022-09-08 14:20:24 +1000

    Update Fruit

--- src/test/java/org/umer/FruitsResourceTest.java
+++ src/test/java/org/umer/FruitsResourceTest.java
@@ -55,4 +55,26 @@ public class FruitsResourceTest {
 
     }
 
+
+    @Test
+    public void updateFruitTest() throws Exception {
+
+        Fruit testFruit = new Fruit("test3", "testRedApple3","red is test3");
+        fruitService.add(testFruit);
+
+        Fruit fruitInserted = fruitService.findById(testFruit.getId());
+
+        Assertions.assertEquals(fruitInserted.getName(),testFruit.getName());
+
+
+        fruitInserted.setName("updated");
+
+        Fruit updatedFruit = fruitService.update(fruitInserted);
+        Assertions.assertEquals("updated" ,updatedFruit.getName());
+
+
+
+
+    }
+
 }
